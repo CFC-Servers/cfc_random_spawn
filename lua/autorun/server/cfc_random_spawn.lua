70cdb13bbe2bcc9e7cc2694a03d26ab283e114f2
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-04-07 20:48:09 -0400

    Fix autorun

--- lua/autorun/server/cfc_random_spawn.lua
+++ lua/autorun/server/cfc_random_spawn.lua
@@ -1,10 +1,5 @@
 CFCRandomSpawn = CFCRandomSpawn or {}
 CFCRandomSpawn.Config = CFCRandomSpawn.Config or {}
 
-AddCSLuaFile "cfc_random_spawn/sh_config.lua"
-
-include "cfc_random_spawn/sh_config.lua"
-
-if SERVER then
-    include "cfc_random_spawn/module/sv_player_spawning.lua"
-end
+include "cfc_random_spawn/sv_config.lua"
+include "cfc_random_spawn/module/sv_player_spawning.lua"
