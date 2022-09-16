8d22dadd71c974a0986f1af3d59d882475173777
Redox <mail@redox.to>
2022-09-16 21:58:12 +0200

    Update spawnpoints realtime

--- lua/cfc_random_spawn/module/sv_spawns_editor.lua
+++ lua/cfc_random_spawn/module/sv_spawns_editor.lua
@@ -1,11 +1,23 @@
 util.AddNetworkString( "CFC_SpawnEditor_SendSpawnPoints" )
 util.AddNetworkString( "CFC_SpawnEditor_RequestSpawnPoints" )
+util.AddNetworkString( "CFC_SpawnEditor_UpdateSpawnPoints" )
 
 net.Receive( "CFC_SpawnEditor_RequestSpawnPoints", function( _, ply )
     if not ply:IsAdmin() then return end
     local customSpawnConfigForMap = CFCRandomSpawn.Config.CUSTOM_SPAWNS[game.GetMap()]
 
+    if not customSpawnConfigForMap then return end
+
     net.Start( "CFC_SpawnEditor_SendSpawnPoints" )
     net.WriteTable( customSpawnConfigForMap )
     net.Send( ply )
+end )
+
+net.Receive( "CFC_SpawnEditor_UpdateSpawnPoints", function( _, ply )
+    if not ply:IsAdmin() then return end
+    local newSpawnTable = net.ReadTable()
+    if not newSpawnTable then return end
+    PrintTable( newSpawnTable )
+    CFCRandomSpawn.Config.CUSTOM_SPAWNS[game.GetMap()] = newSpawnTable
+    CFCRandomSpawn.updateMapSpawns()
 end )
