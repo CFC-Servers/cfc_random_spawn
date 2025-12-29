34aceb17a069e38b2b04b2de9c16ae83a9643212
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2025-12-28 21:52:32 -0700

    gm_novenka > gm_novenka_improved

--- lua/cfc_random_spawn/configs/gm_novenka_night.lua
+++ lua/cfc_random_spawn/configs/gm_novenka_night.lua
@@ -1,2 +1,2 @@
-include( "gm_novenka.lua" )
-CFCRandomSpawn.Config.CUSTOM_SPAWNS["gm_novenka_night"] = CFCRandomSpawn.Config.CUSTOM_SPAWNS["gm_novenka"]
+include( "gm_novenka_improved.lua" )
+CFCRandomSpawn.Config.CUSTOM_SPAWNS["gm_novenka_night"] = CFCRandomSpawn.Config.CUSTOM_SPAWNS["gm_novenka_improved"]
