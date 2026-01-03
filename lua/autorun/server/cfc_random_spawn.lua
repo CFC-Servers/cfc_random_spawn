d6643b5671959708b3f61da7f739c49c194c7382
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2026-01-02 21:27:47 -0700

    OH

--- lua/autorun/server/cfc_random_spawn.lua
+++ lua/autorun/server/cfc_random_spawn.lua
@@ -4,8 +4,5 @@ CFCRandomSpawn.Config = CFCRandomSpawn.Config or {}
 AddCSLuaFile( "cfc_random_spawn/module/cl_spawns_editor.lua" )
 
 include( "cfc_random_spawn/sv_config.lua" )
-
-hook.Add( "InitPostEntity", "CFCRandomSpawn_Init", function()
-    include( "cfc_random_spawn/module/sv_player_spawning.lua" )
-    include( "cfc_random_spawn/module/sv_spawns_editor.lua" )
-end )
+include( "cfc_random_spawn/module/sv_player_spawning.lua" )
+include( "cfc_random_spawn/module/sv_spawns_editor.lua" )
